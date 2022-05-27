cc71d0967eb9a2aaa9fb089e9e3032d1de0ea184
unknown <salimiyahya549@gmail.com>
2022-05-27 16:12:41 +0300

    fix main pain when user is not involved in any course

--- frontend/src/views/DashboardMain.vue
+++ frontend/src/views/DashboardMain.vue
@@ -3,7 +3,7 @@
     <v-row>
       <v-col sm="8" md="6" lg="4" xs="10">
         <p class="text-subtitle-2">Recently active courses</p>
-        <v-carousel v-model="model" cycle :show-arrows="false" progress progress-color="success">
+        <v-carousel cycle :show-arrows="false" progress progress-color="success"  v-if="atLeastOneItem">
           <v-carousel-item
             v-for="course in ongoings"
             :key="course._id"
@@ -13,6 +13,7 @@
             <CourseCard :code="course.code"></CourseCard>
           </v-carousel-item>
         </v-carousel>
+        <p v-else class="text-subtitle-2">You are not involved in any course yet!</p>
       </v-col>
       <v-col md="3"></v-col>
     </v-row>
@@ -30,6 +31,9 @@ export default {
       ...mapGetters(['allCourses']),
       ongoings() {
         return this.allCourses.filter(c => c.state === 'ongoing')
+      },
+      atLeastOneItem() {
+        return this.ongoings().length > 0
       }
     },
     created() {

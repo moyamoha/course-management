c8d5418c3780b307f2d894793e405652ae330b97
unknown <salimiyahya549@gmail.com>
2022-05-27 19:33:10 +0300

    hide only .env.development

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,7 +5,8 @@ import store from './store'
 import vuetify from './plugins/vuetify'
 import axios from 'axios'
 
-axios.defaults.baseURL = process.env.BACKEND_BASE_URL
+axios.defaults.baseURL =
+	process.env.BACKEND_BASE_URL || 'https://coumanager-app.herokuapp.com/api/v1'
 axios.interceptors.request.use((request) => {
 	const token = localStorage.getItem('token')
 	if (token) {

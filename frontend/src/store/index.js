f9fb06ea62fc7ac519a31f7ee9494a896f72eaf3
unknown <salimiyahya549@gmail.com>
2022-06-21 17:22:20 +0300

    add vuex-persist plugin

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,14 +1,20 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VuexPersistence from 'vuex-persist'
 
 import auth from './modules/auth'
 import courses from './modules/courses'
 
 Vue.use(Vuex)
 
+const vuexLocal = new VuexPersistence({
+	storage: window.localStorage,
+})
+
 export default new Vuex.Store({
 	modules: {
 		courses,
 		auth,
 	},
+	plugins: [vuexLocal.plugin],
 })

bb885566a9666fc52b11a1e23311e1b5aa639382
Yahya Salimi <yahya.salimi@eduhouse.fi>
2022-11-21 10:23:16 +0200

    changes

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,26 +1,25 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify'
-import axios from 'axios'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
+import axios from "axios";
 
-axios.defaults.baseURL =
-	process.env.BACKEND_BASE_URL || 'https://coumanager-app.herokuapp.com/api/v1'
+axios.defaults.baseURL = process.env.BACKEND_BASE_URL;
 axios.interceptors.request.use((request) => {
-	const token = localStorage.getItem('token')
-	if (token) {
-		request.headers = {
-			Authorization: `Bearer ${token}`,
-		}
-	}
-	return request
-})
-Vue.config.productionTip = false
+  const token = localStorage.getItem("token");
+  if (token) {
+    request.headers = {
+      Authorization: `Bearer ${token}`,
+    };
+  }
+  return request;
+});
+Vue.config.productionTip = false;
 
 new Vue({
-	router,
-	store,
-	vuetify,
-	render: (h) => h(App),
-}).$mount('#app')
+  router,
+  store,
+  vuetify,
+  render: (h) => h(App),
+}).$mount("#app");
